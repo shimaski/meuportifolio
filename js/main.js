bb903455ceb7885b11f24d74680dc8abc876a385
Adriel Shimaski <adriel889alves@gmail.com>
2022-07-26 00:24:05 -0300

    atualizacao

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 /*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab=>{
+
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tc =>{ /* tc= tab content */
+            tc.classList.remove('filtros__ativado')
+        })
+        target.classList.add('filtros__ativado')
+    })
+})
 
 /*=============== DARK LIGHT THEME ===============*/
 
